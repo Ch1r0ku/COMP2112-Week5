4f94163491f02685d8892b81650f3a3c3e4df88a
Ch1r0ku <romanlysenkowork@gmail.com>
2022-09-12 10:55:41 -0400

    added 3rd party libraries

---
+++ Scripts/app.js
@@ -0,0 +1,11 @@
+//IIFE Immidiately Invoked Function Expression
+
+(function()
+{
+
+    function Start()
+    {
+        console.log("App Started!");
+    }
+    window.addEventListener("load",Start);
+})();
