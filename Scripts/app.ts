e99a6f33c75264be62b87c794263d75a7189e1bc
Ch1r0ku <romanlysenkowork@gmail.com>
2022-09-26 14:42:30 -0400

    added saving and loading functions

--- Scripts/app.ts
+++ Scripts/app.ts
@@ -2,18 +2,13 @@
 //IIFE Immidiately Invoked Function Expression
 (function()
 {
-    // First method of using functions
 
-    function Start()
-    {
-        console.log("App Started!");
-
-        let contactList;
-
-        $.getJSON("./Data/contacts.json", function(DataSource){
-            //Get your data from the DataSource
-        contactList = DataSource.ContactList;     
-
+    /**
+     *This method saves our data to localStorage
+     *
+     * @param {any[]} contactList
+     */
+    function SaveContactListData(contactList: any){
         let count = 0;
         for (const contact of contactList) 
         {
@@ -21,14 +16,47 @@
             localStorage.setItem(count.toString(), newContact.toJSON());
             count++;
         }
+    }
+
+    /**
+     *This method reads our data from localStorage and returns a Contact Array
+     *
+     * @returns  {Contact[]}
+     */
+    function LoadContactListData():Contact[]
+    {
+        //create an empty Contact Array container
+        let ContactArray = new Array<Contact>();
 
         let keys = Object.keys(localStorage);
         for (let key of keys) 
         {
             let newContact = new Contact();
             newContact.fromJSON(localStorage.getItem(key));
-            console.log(newContact.toString());
+            ContactArray.push(newContact);
         }
+        return ContactArray;
+    }
+
+    // First method of using functions
+
+    function Start()
+    {
+        console.log("App Started!");
+
+
+        $.getJSON("./Data/contacts.json", function(DataSource){
+            //Get your data from the DataSource
+        let contactList:any[] = DataSource.ContactList;    
+        
+        SaveContactListData(contactList);
+
+        let ContactArray = LoadContactListData();
+
+        for (const contact of ContactArray) {
+            console.log(contact.toString())
+        }
+       
     });
     
 
